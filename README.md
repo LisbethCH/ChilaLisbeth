1420497fcb16ec6a881d82ff8e4eac8646b7777e
LisbethCH <132473565+LisbethCH@users.noreply.github.com>
2023-05-03 21:28:43 -0500

    chila archivos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ChilaLisbeth
+# FrancisStalin
+actividad del primer parcila de gestion del software
---
+++ marco-central.php
@@ -0,0 +1,64 @@
+<div style="float:left; width:60%; margin:60px auto; padding:1vw; border-left:1px solid gray; border-right:1px solid gray;">
+   <div  style="width: 100%;  ">     
+    <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
+       <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left; ">
+        	<h1>¿Quién es Lisbeth jasmin Chila Napa?</h1>
+       		 <hr>
+             
+          <div id="posteo" style="text-align: left; font-size:0.5vw;">
+                Posteado por:Lisbeth jasmin Chila Napa<br>
+                Fecha: 2023-05-03 <br>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div style="width: 100%; height:100%;" >
+           <div  style="text-align:justify; font-size: 20px;">
+                   <p class="eys-parrafo">Hola,  Lisbeth   <b> Chila</b> ,  estudiante en la UTELVT en septimo ciclo de la carrera de Tecnología de la Información.  
+          Mis aspiraciones son poder aprender cada día de lo que respecta a mi carrera, ya que esto me servirá de gran ayuda para mi futuro para poder trabajar y poder darle un mejor futuro a mi hijo. </p>
+                   
+ 
+
+   <div id="blog-seccion2"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
+    
+
+
+                   <p class="eys-parrafo">Me gradué el 21 de febrero del 2019 en la unidad Educativa Muisne cuando me gradué decidí estudiar una carrrera por lo cual postulé para la universidad de Esmeraldas Técnica Luis Vargas torres para la carrera de in. En tecnologías.</p>
+                   <p class="eys-parrafo"> Tengo un carácter Fuerte y muchas de las veces que actuó lo hago sin pensar, aunque luego suelo arrepentirme en la mayoría de los casos, Claro... cuando mis actos no son favorables para los que me rodean, porque se que mientras tenga que tomar decisiones mas riesgosas, suelo analizar mas de diez veces y probar miles de posibilidades. </p>
+                    <p  class="eys-parrafo"> siempre pero siempre intento lograr sus sueños , siempre he considerado mis fracasos como impulsos de mis éxitos. Soy Responsable, un poco estudiosa ( nunca leí un libro para estudiar algún examen o prueba al día siguiente) y no me gusta depender de nadie nunca. </p> 
+                   
+ 
+   <div id="blog-seccion3"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
+    
+                   <p  class="eys-parrafo">poseo conocimientos de HTML, CSS para crear paginas web de acuerdo a las necesidades que me soliciten  </p> 
+                   
+                   
+   <div id="blog-seccion4"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
+    
+
+                   <p   class="eys-parrafo">Soy Responsable, un poco estudiosa ( nunca leí un libro para estudiar algún examen o prueba al día siguiente) y no me gusta depender de nadie nunca. Pero me gusta cantar y bailar en mis tiempo libre </p>
+       
+
+                   
+
+               <hr>
+               <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
+                  <div style="width: 40%;  float: left;">
+                   <a href="#">&#171;   Previo</a>
+                  </div>
+
+                  <div style="width: 40%; float: right; text-align: right;">
+                   <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
+                  </div>
+
+                </div>
+           </div>
+  </div> 
+
+  </div>
